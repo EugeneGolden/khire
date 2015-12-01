83a9a857db189c45b3144c60c83db48e544153a5
Eugene <yeug@gmail.com>
2015-12-01 21:52:38 +0200

    3 programmes

---
+++ skier04/skier04/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace skier04
+{
+    class Program
+    {
+        //Начав тренировки, лыжник в первый день пробежал 10 км. Каждый следующий день он увеличивал пробег на 10% от пробега предыдущего дня.
+        //Определить, какой суммарный путь он пробежал за первые 7 дней тренировок.
+
+        public decimal Distance(decimal firstDayDistance, decimal koefficient, int days)
+        {
+            decimal summa = 0;
+            
+            for (int i = 0; i < days; i++)
+            {
+                summa = firstDayDistance + koefficient * summa;
+                Console.WriteLine("Sum of " + (i + 1) + " days is " + summa);
+                Console.ReadKey();
+            }
+            return summa;
+        }
+
+        static void Main(string[] args)
+        {
+            decimal firstDayDistance = 10;
+            decimal koefficient = (decimal)1.1;
+            int days = 7;
+
+            decimal resultat;
+            Program res = new Program();
+            resultat = res.Distance(firstDayDistance, koefficient, days);
+        }
+    }
+}
+
+
